5ee1ce1de641a13f747ee530edb3e186a80eada4
Brian Cook <brybcook@gmail.com>
2021-02-18 17:28:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,9 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  collection.each do |key, value|
+    if value == name
+      return name
 end
 
 def consolidate_cart(cart)
