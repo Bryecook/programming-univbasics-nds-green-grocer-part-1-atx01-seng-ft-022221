34c8283cfb9d1c6b332c8ca765a714bbc617c571
Brian Cook <brybcook@gmail.com>
2021-02-18 17:34:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,12 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
   collection.each do |key, value|
     if key==:item
       if value==name
         return name
-      elsif
-      nil
     end
   end 
   end
